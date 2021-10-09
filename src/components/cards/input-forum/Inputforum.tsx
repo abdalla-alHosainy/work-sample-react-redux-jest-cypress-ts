f0086c9a0d24ab67a8643055f64c49804eca8492
abdalla-alHosainy <abdallaelhosini@gmail.com>
2021-10-09 16:49:18 +0200

    master

---
+++ src/components/cards/input-forum/Inputforum.tsx
@@ -0,0 +1,15 @@
+import React from "react"
+import styled from "@emotion/styled"
+import theme from "../../../style"
+
+const color = theme.cards.color
+function Inputforum() {
+  return <div></div>
+}
+const Holder = styled.div`
+  display: flex;
+  flex-direction: column;
+  /* align-items: flex-start; */
+`
+
+export default Inputforum
---
+++ src/style/index.ts
@@ -0,0 +1,27 @@
+const theme = {
+  app: {
+    color: {
+      black: "#1D1D1D",
+      ofWhite: "#E2E2E2",
+      white: "#fff",
+      gray: "#AFAFAF",
+      blue: "#253E7D",
+      violet: "#e3ef",
+    },
+  },
+  hero: {},
+  cards: {
+    color: {
+      white: "#fff",
+      offWhite: "#E2E2E2",
+      gray: "#AFAFAF",
+      black: "#1D1D1D",
+    },
+    font: {
+      regular: "",
+    },
+  },
+  gantt: {},
+}
+
+export default theme
